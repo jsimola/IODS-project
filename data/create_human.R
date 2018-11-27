31e74f94a637b54cc1892d5da72900c6cf5f2c66
Jaana Simola <jsimola@staff-45.vpn.helsinki.fi>
2018-11-27 18:33:09 +0200

    update create_human -script

--- data/create_human.R
+++ data/create_human.R
@@ -3,6 +3,11 @@
 # Data wrangling - 3rd week
 # Link to the metadata (http://hdr.undp.org/en/content/human-development-index-hdi)
 
+rm(list = ls())
+setwd("~/Documents/GitHub/IODS-project/data") 
+library(dplyr) # access the dplyr library
+library(stringr)
+
 # Load datasets
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
 gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
@@ -16,34 +21,69 @@ summary(hd)
 summary(gii)
 
 # Rename the variables with (shorter) descriptive names
-colnames(hd)[3] <- "HDI"    # Human Development Index
-colnames(hd)[4] <- "Life.expectancy"
-colnames(hd)[5] <- "edu.expectancy"
-colnames(hd)[6] <- "edu.inYears"
-colnames(hd)[7] <- "GNI"    # Gross National Income
-colnames(hd)[8] <- "GNI_minus_HDI"
-
-colnames(gii)[3] <- "GII" # Gender Inequality Index
-colnames(gii)[4] <- "MMR" # Maternal Mortality Ratio
-colnames(gii)[5] <- "ABR" # Adolescent Birth Rate
-colnames(gii)[6] <- "F.inParl" # Share of seats in parliament held by women
-colnames(gii)[7] <- "edu2F" # Percentage of women (> 25 yrs old) with secondary education
-colnames(gii)[8] <- "edu2M"  # Percentage of men (> 25 yrs old) with secondary education
-colnames(gii)[9] <- "workF" # Percentage of women (> 15 yrs old) participating in labour 
-colnames(gii)[10] <- "workM" # Percentage of men (> 15 yrs old) participating in labour 
-
-
+colnames(hd) <- c("HDI.Rank", "Country", "HDI", "life.expectancy", "edu.expectancy", "edu.inYears", "GNI", "GNI-HDI")
+colnames(gii) <- c("GII.Rank", "Country", "GII", "MMR", "ABR", "F.inParl", "edu2F", "edu2M", "workF", "workM")
+#colnames(hd)[3] <- "HDI"    # alternative way of changing the column names
 
 # create two new variables to gii. 
 gii <- mutate(gii, eduRatio = edu2F / edu2M) # the ratio of Female and Male populations with secondary education 
 gii <- mutate(gii, workRatio = workF / workM) # the ratio of labour force participation of females and males 
 
-# access the dplyr library
-library(dplyr)
-
-
 #Join together the two datasets using the variable Country as the identifier. 
-human <- inner_join(gii, hd, by = "Country") # 195 observations and 19 variables. 
+human <- inner_join(hd, gii, by = "Country") # 195 observations and 19 variables. 
 
 # Call the new joined data "human" and save it in your data folder.
 write.csv(human, file="human.csv", row.names = FALSE) # write data to file
+
+# ------------------------------------------------
+
+
+str(human) # the 'human' data includes 195 observations and 19 variables that combine health and education indicators from most countries in the world
+
+# Description of 'human' dataset variables. 
+# "HDI.Rank" = Country rank in Human Development Index
+# "Coutry"
+# "HDI" = Human Development Index
+# "life.expectancy" = Life expectancy at birth
+# "edu.expectancy" = Expected years of schooling
+# "edu.inYears" = Mean years of schooling
+# "GNI" = Gross national income (GNI) per capita
+# "GNI-HDI" = Gross national income (GNI) per capita - Human Development Index
+# "GII.Rank" = Country rank in Gender Inequality Index
+# "GII" = Gender Inequality Index
+# "MMR" = Maternal mortality ratio
+# "ABR" = Adolescent birth rate
+# "F.inParl" = Percetange of female representatives in parliament 
+# "edu2F" = Population of women with at least some secondary education
+# "edu2M" = Population of men with at least some secondary education
+# "workF" = Population of women participating in labor
+# "workM" = Population of men participating in labor
+# "eduRatio" = The ratio of Female / Male populations with secondary education (edu2F / edu2M)
+# "workRatio" = The ratio of Female / Male participation in labour force (work2F / work2M)  
+
+
+# transform the Gross National Income (GNI) variable to numeric
+human$GNI <- str_replace(human$GNI, ",", "")
+human$GNI <- as.numeric(human$GNI)
+
+# keep only these columns
+keep <- c("Country", "eduRatio", "workRatio", "edu.expectancy", "life.expectancy", "GNI", "MMR", "ABR", "F.inParl")
+human <- dplyr::select(human, one_of(keep))
+
+# remove rows with missing values 
+complete.cases(human)
+data.frame(human[-1], comp = complete.cases(human))
+human <- filter(human, complete.cases(human))
+
+# remove regions that comprise the last 7 rows
+tail(human, n=10L)
+last <- nrow(human) - 7
+human_ <- human[1:last, ]
+
+
+rownames(human_) <- human_$Country # add countries as rownames
+human_ <- dplyr::select(human_, -Country) # remove the Country variable
+dim(human_) # 155 observations and 8 variables
+
+write.csv(human_, file="human.csv", row.names = TRUE) # write data to file
+# test <- read.csv(file="human.csv")
